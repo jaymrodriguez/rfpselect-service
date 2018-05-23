97b0fe1b2f8e765364a2a441db6a8ded7c9de592
Randy Perez <randyjp125@gmail.com>
2018-05-23 14:52:39 -0400

    adding prettier integration

--- service-layer/handlers/errorHandlers.js
+++ service-layer/handlers/errorHandlers.js
@@ -1,8 +1,7 @@
-exports.catchErrors = (fn) => {
-    return function (req, res, next) {
-        return fn(req, res, next).catch(next);
-    };
-};
+exports.catchErrors = fn =>
+  function (req, res, next) {
+    return fn(req, res, next).catch(next);
+  };
 
 /*
   Development Error Hanlder
@@ -10,22 +9,21 @@ exports.catchErrors = (fn) => {
   In development we show good error messages so if we hit a syntax error or any other previously un-handled error, we can show good info on what happened
 */
 exports.developmentErrors = (err, req, res, next) => {
-    err.stack = err.stack || '';
-    const errorDetails = {
-        message: err.message,
-        status: err.status,
-        stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>')
-    };
-    res.status(err.status || 500);
-    res.format({
-        // Based on the `Accept` http header
-        'text/html': () => {
-            res.render('error', errorDetails);
-        }, // Form Submit, Reload the page
-        'application/json': () => res.json(errorDetails) // Ajax call, send JSON back
-    });
+  err.stack = err.stack || '';
+  const errorDetails = {
+    message: err.message,
+    status: err.status,
+    stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>'),
+  };
+  res.status(err.status || 500);
+  res.format({
+    // Based on the `Accept` http header
+    'text/html': () => {
+      res.render('error', errorDetails);
+    }, // Form Submit, Reload the page
+    'application/json': () => res.json(errorDetails), // Ajax call, send JSON back
+  });
 };
-
 
 /*
   Production Error Hanlder
@@ -33,9 +31,9 @@ exports.developmentErrors = (err, req, res, next) => {
   No stacktraces are leaked to user
 */
 exports.productionErrors = (err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
 };
