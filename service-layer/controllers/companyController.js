579a0ff4959ba5335774b98116a0caa837ffa166
Randy Perez <randyjp125@gmail.com>
2018-05-24 14:30:23 -0400

    adding the rfpselect network to compose

--- service-layer/controllers/companyController.js
+++ service-layer/controllers/companyController.js
@@ -1,16 +1,14 @@
 const WPAPI = require('wpapi');
 
 exports.getCompanies = async (req, res) => {
-  const wp = await WPAPI.discover('http://localhost:8000/');
+  const wp = await WPAPI.discover('http://0.0.0.0:8000/');
   const companies = await wp.company();
   res.json(companies);
 };
 
 exports.getCompanyById = async (req, res) => {
   const { id } = req.params;
-  console.log(id);
-  console.log('mmg');
-  const wp = await WPAPI.discover('http://localhost:8000/');
+  const wp = await WPAPI.discover('http://0.0.0.0:8000/');
   const company = await wp.company().id(id);
   res.json(company);
 };
