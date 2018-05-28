f88489b2bab4dbbb099b8c8f3853a3b3cfb6a8df
Randy Perez <randyjp125@gmail.com>
2018-05-28 13:58:07 -0400

    setting up endpoint to create company

--- service-layer/app.js
+++ service-layer/app.js
@@ -1,35 +1,35 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const WP_API = require('./api/api');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const WP_API = require("./api/api");
 
-const routes = require('./routes/index');
+const routes = require("./routes/index");
 // const usersRouter = require('./routes/users');
 // const companiesRouter = require('./routes/companies');
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // pass api variables to templates and response
 app.use((req, res, next) => {
-  req.api = new WP_API('wp/v2', 'http://wp/wp-json');
+  req.api = new WP_API("wp/v2", "http://wp/wp-json");
   // res.api = new WP_API();
   next();
 });
 
-app.use('/', routes);
+app.use("/", routes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -40,11 +40,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- service-layer/controllers/companyController.js
+++ service-layer/controllers/companyController.js
@@ -10,22 +10,18 @@ exports.getCompanies = async (req, res) => {
 };
 
 exports.createCompany = async (req, res) => {
-  // const company = {
-  //   title: 'The Apple',
-  //   name: 'Apple',
-  //   url: 'https://www.apple.com/',
-  //   founding_date: new Date('04/04/2015'),
-  //   size_of_organization: '1000',
-  //   description: 'The best of the best',
-  // };
-  const comp = await req.api.wp.companies().create({
+  const companyInfo = {
     title: req.body.title,
-    name: req.body.name,
-    url: req.body.url,
-    founding_date: req.body.founding_date,
-    size_of_organization: req.body.size_of_organization,
-    description: req.body.description,
-    // logo: company.logo,
-  });
-  res.json(comp);
+    status: "publish",
+    fields: {
+      name: req.body.name,
+      url: req.body.url,
+      founding_date: req.body.founding_date,
+      size_of_organization: req.body.size_of_organization,
+      description: req.body.description
+      // logo: company.logo,
+    }
+  };
+  const company = await req.api.wp.companies().create(companyInfo);
+  res.json(company);
 };
