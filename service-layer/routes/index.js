b5bdc4d08712b41e4ac5cab5a30d9c3ac833eaaf
Randy Perez <randyjp125@gmail.com>
2018-05-28 14:04:51 -0400

    editting route for adding a company

--- service-layer/routes/index.js
+++ service-layer/routes/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const companyController = require('../controllers/companyController');
-const { catchErrors } = require('../handlers/errorHandlers');
+const companyController = require("../controllers/companyController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+  res.render("index", { title: "Express" });
 });
 
-router.get('/companies', catchErrors(companyController.getCompanies));
-router.get('/companies/:id', catchErrors(companyController.getCompanyById));
-router.post('/create', catchErrors(companyController.createCompany));
+router.get("/companies", catchErrors(companyController.getCompanies));
+router.get("/companies/:id", catchErrors(companyController.getCompanyById));
+router.post("/companies/add", catchErrors(companyController.createCompany));
 
 module.exports = router;
