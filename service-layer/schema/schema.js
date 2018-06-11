7e63d71766850dee84d0f58df1edf3307dd2d0ad
Randy Perez <randyjp125@gmail.com>
2018-06-11 10:52:12 -0400

    adding data sanitization

--- service-layer/schema/schema.js
+++ service-layer/schema/schema.js
@@ -7,36 +7,56 @@ const {
 } = require("../helpers/helpers");
 
 exports.companySchema = [
-  check("title").isLength({ min: 1, max: 255 }),
-  check("name").isLength({ min: 1, max: 255 }),
+  check("title")
+    .isLength({ min: 1, max: 255 })
+    .escape()
+    .trim(),
+  check("name")
+    .isLength({ min: 1, max: 255 })
+    .escape()
+    .trim(),
   check("url").isURL(),
   check("founding_date").custom(isValidDate),
   check("size_of_organization").isInt({ gt: 3 }),
-  check("description").isLength({ min: 1 }),
+  check("description")
+    .isLength({ min: 1 })
+    .escape()
+    .trim(),
   check("categories").custom(isArrayOrInt),
   check("resourcing").custom(isArrayOrInt),
   check("technologies").custom(isArrayOrInt)
 ];
 
 exports.locationSchema = [
-  check("title").isLength({ min: 1, max: 255 }),
+  check("title")
+    .isLength({ min: 1, max: 255 })
+    .escape()
+    .trim(),
   check("company_id").custom((value, { req }) => companyExists(value, req)),
   check("address")
     .not()
-    .isEmpty(),
+    .isEmpty()
+    .escape()
+    .trim(),
   check("city")
     .isAlpha()
     .not()
-    .isEmpty(),
+    .isEmpty()
+    .escape()
+    .trim(),
   check("zip_code")
     .isAlphanumeric()
     .not()
-    .isEmpty(),
+    .isEmpty()
+    .escape()
+    .trim(),
   check("country").isISO31661Alpha2(),
   check("state_region")
     .isAlpha()
     .not()
-    .isEmpty(),
+    .isEmpty()
+    .escape()
+    .trim(),
   check("is_hq")
     .isBoolean()
     .custom((value, { req }) => companyHasHQ(value, req))
