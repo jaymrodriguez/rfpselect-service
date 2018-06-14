8f0576717ff177d68f84b626021f590e33d55ce4
Randy Perez <randyjp125@gmail.com>
2018-06-14 15:15:59 -0400

    Setting up react router and basic component structure

---
+++ front-end/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
+
+import './App.css';
+
+import companyForm from '../components/companyForm';
+
+class App extends Component {
+  render() {
+    return (
+      <Router className="App">  
+        <Switch>
+          <Route exact path='/' component={companyForm}/>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
