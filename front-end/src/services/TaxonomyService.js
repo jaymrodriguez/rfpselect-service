3c838bad1134ebe79d8f1c79896872e59dad572d
Randy Perez <randyjp125@gmail.com>
2018-06-20 11:18:01 -0400

    includign template css files

--- front-end/src/services/TaxonomyService.js
+++ front-end/src/services/TaxonomyService.js
@@ -1,31 +1,7 @@
 import axios from 'axios';
 
-// export const getResourcing = async () => {
-//   const resourcing = await axios.get('/taxonomies/resourcing');
-//   return resourcing.data;
-// };
+export const getResourcing = async () => axios.get('/taxonomies/resourcing');
 
-// export const getTechnologies = async () => {
-//   const technologies = await axios.get('/taxonomies/technologies');
-//   return technologies.data;
-// };
+export const getTechnologies = async () => axios.get('/taxonomies/technologies');
 
-// export const getCategories = async () => {
-//   const categories = await axios.get('/taxonomies/categories');
-//   return categories.data;
-// };
-
-export const getResourcing = async () => {
-  const resourcing = axios.get('/taxonomies/resourcing');
-  return resourcing;
-};
-
-export const getTechnologies = async () => {
-  const technologies = axios.get('/taxonomies/technologies');
-  return technologies;
-};
-
-export const getCategories = async () => {
-  const categories = axios.get('/taxonomies/categories');
-  return categories;
-};
+export const getCategories = async () => axios.get('/taxonomies/categories');
