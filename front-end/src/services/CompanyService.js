646150acc9887d7982602c4406553b318bbf9a35
Randy Perez <randyjp125@gmail.com>
2018-06-26 16:59:36 -0400

    using base url for company service

--- front-end/src/services/CompanyService.js
+++ front-end/src/services/CompanyService.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import qs from 'qs';
 
+const baseURL = process.env.REACT_APP_PROXY;
+
 export const test = () => {};
 
 export const createCompany = async (companyInfo) => {
   const params = qs.stringify(companyInfo, { indices: false });
   try {
-    const response = await axios.post('/companies/add', params);
+    const response = await axios.post(`${baseURL}/companies/add`, params);
     return response;
   } catch (error) {
     return error.response;
