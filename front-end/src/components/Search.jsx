d6b211a45a51935089f9a80d492e48fc8b1f4515
Randy Perez <randyjp125@gmail.com>
2018-07-20 15:06:32 -0400

    first draft of a workign search

--- front-end/src/components/Search.jsx
+++ front-end/src/components/Search.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { ReactiveBase, DataSearch } from '@appbaseio/reactivesearch';
+import {
+  ReactiveBase,
+  DataSearch,
+  CategorySearch,
+  MultiList,
+  ResultCard,
+} from '@appbaseio/reactivesearch';
 
 class Search extends React.Component {
   state = {};
@@ -7,9 +13,28 @@ class Search extends React.Component {
     <ReactiveBase app="rfpindex" url="http://localhost:9200">
       <DataSearch
         componentId="searchbox"
-        dataField="title"
-        categoryField="title"
-        placeholder="Search Companiesxx"
+        dataField="content_searchable"
+        onSuggestion={suggestion => ({
+          label: <div>{suggestion._source.title}</div>,
+          value: suggestion._source.title,
+        })}
+        placeholder="Serch for Companies"
+      />
+      <ResultCard
+        componentId="result"
+        title="Results"
+        dataField="content_searchable"
+        from={0}
+        size={5}
+        pagination
+        react={{
+          and: ['searchbox'],
+        }}
+        onData={res => ({
+          image: 'https://bit.do/demoimg',
+          title: res.title,
+          description: res.description_str,
+        })}
       />
     </ReactiveBase>
   );
