d47433c1f931bec28fe15a7ac01d462643d11519
Randy Perez <randyjp125@gmail.com>
2018-07-24 10:23:54 -0400

    change years in business filter to a dropdown

--- front-end/src/components/Search.jsx
+++ front-end/src/components/Search.jsx
@@ -1,3 +1,5 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_source"] }] */
+
 import React from 'react';
 import moment from 'moment';
 import {
@@ -5,8 +7,15 @@ import {
   DataSearch,
   ResultCard,
   MultiDropdownList,
-  SingleRange,
+  SingleDropdownRange,
 } from '@appbaseio/reactivesearch';
+
+const sugggestions = [
+  { label: 'Intellisys', value: 'Intellisys' },
+  { label: 'PHP', value: 'PHP' },
+  { label: 'Content Management System', value: 'CMS' },
+  { label: 'JavaScript', value: 'JavaScript' },
+];
 
 const dateFilterValues = [
   { start: 150, end: 0, label: 'Any' },
@@ -29,76 +38,69 @@ const yearsQuery = dateRange => ({
     },
   },
 });
-const Search = () => {
-  const sugggestions = [
-    { label: 'Intellisys', value: 'Intellisys' },
-    { label: 'PHP', value: 'PHP' },
-    { label: 'Content Management System', value: 'CMS' },
-    { label: 'JavaScript', value: 'JavaScript' },
-  ];
-  return (
-    <ReactiveBase app="rfpindex" url="http://localhost:9200">
-      <DataSearch
-        componentId="searchbox"
-        dataField="content_searchable"
-        onSuggestion={suggestion => ({
-          label: <div>{suggestion._source.title}</div>,
-          value: suggestion._source.title,
-        })}
-        placeholder="Serch for Companies"
-        defaultSuggestions={sugggestions}
-      />
-      <MultiDropdownList
-        componentId="resourcing_filter"
-        dataField="resourcing_str"
-        title="Resourcing"
-        showSearch={false}
-        react={{ and: ['searchbox'] }}
-      />
-      <MultiDropdownList
-        componentId="technology_filter"
-        dataField="technologies_str"
-        title="Technologies"
-        showSearch={false}
-        react={{ and: ['searchbox'] }}
-      />
-      <MultiDropdownList
-        componentId="categories_filter"
-        dataField="categories_str"
-        title="Categories"
-        showSearch={false}
-        react={{ and: ['searchbox'] }}
-      />
-      <SingleRange
-        componentId="date_filter"
-        dataField="founding_date_dt"
-        data={dateFilterValues}
-        customQuery={yearsQuery}
-      />
-      <ResultCard
-        componentId="result"
-        title="Results"
-        dataField="content_searchable"
-        from={0}
-        size={8}
-        pagination
-        react={{
-          and: ['searchbox', 'resourcing_filter', 'categories_filter', 'date_filter'],
-        }}
-        onData={res => ({
-          image: 'https://bit.do/demoimg',
-          title: res.title,
-          description: res.description_str,
-          url: `company/${res.id}`,
-        })}
-        defaultQuery={() => ({
-          match: {
-            type: 'company',
-          },
-        })}
-      />
-    </ReactiveBase>
-  );
-};
+const Search = () => (
+  <ReactiveBase app="rfpindex" url="http://localhost:9200">
+    <DataSearch
+      componentId="searchbox"
+      dataField="content_searchable"
+      onSuggestion={suggestion => ({
+        label: <div>{suggestion._source.title}</div>,
+        value: suggestion._source.title,
+      })}
+      placeholder="Serch for Companies"
+      defaultSuggestions={sugggestions}
+    />
+    <MultiDropdownList
+      componentId="resourcing_filter"
+      dataField="resourcing_str"
+      title="Resourcing"
+      showSearch={false}
+      react={{ and: ['searchbox'] }}
+    />
+    <MultiDropdownList
+      componentId="technology_filter"
+      dataField="technologies_str"
+      title="Technologies"
+      showSearch={false}
+      react={{ and: ['searchbox'] }}
+    />
+    <MultiDropdownList
+      componentId="categories_filter"
+      dataField="categories_str"
+      title="Categories"
+      showSearch={false}
+      react={{ and: ['searchbox'] }}
+    />
+    <SingleDropdownRange
+      componentId="date_filter"
+      title="Years in business"
+      dataField="founding_date_dt"
+      data={dateFilterValues}
+      customQuery={yearsQuery}
+    />
+    <ResultCard
+      componentId="result"
+      title="Results"
+      dataField="content_searchable"
+      from={0}
+      size={8}
+      pagination
+      react={{
+        and: ['searchbox', 'resourcing_filter', 'categories_filter', 'date_filter'],
+      }}
+      onData={res => ({
+        image: 'https://bit.do/demoimg',
+        title: res.title,
+        description: res.description_str,
+        url: `company/${res.id}`,
+      })}
+      defaultQuery={() => ({
+        match: {
+          type: 'company',
+        },
+      })}
+    />
+  </ReactiveBase>
+);
 
 export default Search;
